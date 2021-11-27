c416cd2587158f75816fdd335086c3edc341be8a
RudeCMS <94735968+RudeCMS@users.noreply.github.com>
2021-11-27 18:55:08 +1100

    Update bloglist.php

--- src/bloglist.php
+++ src/bloglist.php
@@ -1,4 +1,7 @@
 <?php
 $blogPost[0] = "sample-1";
+$blogPostTime[0] = "1637999663";
 $blogPost[1] = "sample-2";
+$blogPostTime[1] = "1637999654";
 $blogPost[2] = "sample-3";
+$blogPostTime[2] = "1637999679";
