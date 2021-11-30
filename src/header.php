64817e89435455510929b5bba0375aea0e84f933
RudeCMS <94735968+RudeCMS@users.noreply.github.com>
2021-11-30 22:37:32 +1100

    Update header.php
    Add alt text to logo

--- src/header.php
+++ src/header.php
@@ -154,7 +154,7 @@ a:active {
 <div class="container-sm">
 	<div class="row p-2">
 		<div class="col-12 text-center">
-			<a href="<?php echo $website;?>"><img src="<?php echo $website;?>/logo.png" class="d-block mx-auto rounded-circle border border-dark m-2" alt="<?php echo $blogName;?>"></a>
+			<a href="<?php echo $website;?>"><img src="<?php echo $website;?>/logo.png" class="d-block mx-auto rounded-circle border border-dark m-2" alt="<?php echo $blogName;?> Logo"></a>
 			<h1><?php echo $blogName;?></h1>
 			<h2 class="h6"><?php echo $blogSubtitle;?></h2>
 			<?php include 'socials.php';?>
