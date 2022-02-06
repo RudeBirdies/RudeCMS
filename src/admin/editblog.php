75005a8809fe1b106a7e12dcb463a4e368cff47d
Rude Birdies <94735968+RudeCMS@users.noreply.github.com>
2022-02-06 11:31:32 +1100

    Update editblog.php
    Replacing "Empty" with an empty string to allow for blogs without content other than pictures.

--- src/admin/editblog.php
+++ src/admin/editblog.php
@@ -50,7 +50,9 @@ if (empty($slug)) {
 }
 
 if (empty($body)) {
-	$body = 'Empty';
+	/* Replacing "Empty" with an empty string to allow for blogs without content other than pictures.*/
+	/* $body = 'Empty'; */
+	$body = '';
 }
 
 if (empty($metadesc)) {
