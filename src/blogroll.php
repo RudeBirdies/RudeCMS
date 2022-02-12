7a5eaf5743bf1b2d89610c409af672ba508c1ab5
Rude Birdies <94735968+RudeCMS@users.noreply.github.com>
2022-02-12 23:31:12 +1100

    Update blogroll.php
    Adding support for pagination option

--- src/blogroll.php
+++ src/blogroll.php
@@ -6,19 +6,39 @@ include 'bloglist.php';
 $reverseOrder = array_reverse($blogPost);
 
 $numPosts = count($reverseOrder);
+
 $maxPosts = $numPosts;
 
 if ($numPosts > $initialOnScreen) {
 	$numPosts = $initialOnScreen;
 }
 
+// How many have we already shown?
+if (isset($_GET['page']) && $_GET['page'] <> '0') {
+	$startAt = (preg_replace('[\D]', '', $_GET['page']) * $initialOnScreen);
+} else {
+	$startAt = 0;
+}
+
+
+
+
 $i = 0;
 
 while ($i < $numPosts) {
-		
-	include 'blog/'.$reverseOrder[$i].'/index.php';
+	if ($startAt > count($reverseOrder)) {
+		break; // If some wise guy tried putting in a page number higher than our max, then exit.
+	}
+	
+	include 'blog/'.$reverseOrder[$startAt].'/index.php';
 
-$i++;
+	$startAt++;
+	$i++;
+
+	if ($startAt >= count($reverseOrder)) {
+		break; // If we've now hit our max before finishing out loop, then exit.
+	}
+
 
 }
 
